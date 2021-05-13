7c19dfb7c6db2a5a2cc0399e1eab7387103f17d8
Timothy Johnson <60270341+Tjohnzon25@users.noreply.github.com>
2021-05-13 13:00:35 -0700

    Display doesnt work properly yet but everything else does

--- app/src/main/java/com/example/cst438_project3/SavedQuotesDisplay.java
+++ app/src/main/java/com/example/cst438_project3/SavedQuotesDisplay.java
@@ -3,29 +3,46 @@ package com.example.cst438_project3;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.cst438_project3.userDB.UserDAO;
+import com.example.cst438_project3.userDB.UserDatabase;
+import com.parse.ParseUser;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class SavedQuotesDisplay extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener {
 
     RecyclerViewAdapter adapter;
+    UserDAO mUserDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_quotes_display);
 
-        ArrayList<String> quotes = new ArrayList<>();
-        quotes.add("Yoda Quote 1");
-        quotes.add("Yoda Quote 2");
-        quotes.add("Yoda Quote 3");
-        quotes.add("Yoda Quote 4");
-        quotes.add("Yoda Quote 5");
+        getDatabase();
 
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("userName");
+
+        ArrayList<String> quotes = new ArrayList<>();
+
+        List<User> allAccounts = mUserDAO.getUsers();
+
+        for(int i = 0; i < allAccounts.size(); i++){
+            if(allAccounts.get(i).getAppUsername().equals(username)){
+                quotes = allAccounts.get(i).getQuotes();
+                System.out.println(quotes);
+                break;
+            }
+        }
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.rvSavedQuotes);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -36,6 +53,13 @@ public class SavedQuotesDisplay extends AppCompatActivity implements RecyclerVie
 
     @Override
     public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getDatabase() {
+        mUserDAO= Room.databaseBuilder(this, UserDatabase.class,"USER_TABLE")
+                .allowMainThreadQueries()
+                .build()
+                .getUserDAO();
     }
 }
--- app/src/main/java/com/example/cst438_project3/userDB/DateTypeConverters/Converter.java
+++ app/src/main/java/com/example/cst438_project3/userDB/DateTypeConverters/Converter.java
@@ -2,7 +2,15 @@ package com.example.cst438_project3.userDB.DateTypeConverters;
 
 import androidx.room.TypeConverter;
 
+import com.example.cst438_project3.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class Converter {
     @TypeConverter
@@ -13,5 +21,18 @@ public class Converter {
     @TypeConverter
     public Date convertLongtoDate(long time){
         return new Date(time);
+    }
+
+    @TypeConverter
+    public ArrayList<String> fromString(String value) {
+        Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public String fromArrayList(ArrayList<String> list) {
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return json;
     }
 }
